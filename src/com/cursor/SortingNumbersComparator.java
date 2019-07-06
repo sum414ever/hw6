1614006b1fd821d3021b91e340d1942175cc98fb
sum414ever <copokaalekc@gmail.com>
2019-07-06 22:44:05 +0300

    Dev:fixed remark by Vladimir Boldovskyy

--- src/com/cursor/SortingNumbersComparator.java
+++ src/com/cursor/SortingNumbersComparator.java
@@ -5,14 +5,10 @@ import java.util.Comparator;
 public class SortingNumbersComparator implements Comparator<Integer> {
     @Override
     public int compare(Integer n1, Integer n2) {
-        if (n1 % 2 == 0) {
-            if (n2 % 2 == 0) {
-                return 1;
-            }
+        if (Math.abs(n1 % 2) == Math.abs(n2 % 2)) {
+            return n1 - n2;
+        } else if (n1 % 2 == 0) {
             return -1;
-        } else if (n2 % 2 == 0) {
-            return 1;
-        }
-        return 1;
+        } else return 1;
     }
 }
